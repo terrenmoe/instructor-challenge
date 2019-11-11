e8f391be17391083b6722252f91cffdfcfa26f1d
Alex Rossiter <terrenmoe@gmail.com>
2019-11-11 08:57:39 -0800

    Adjusted style, destructuring and empty list item addtion prevention

--- src/App.js
+++ src/App.js
@@ -1,92 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createStore } from 'redux';
 import { connect } from 'react-redux';
 
-// You have learned to use 'import' to bring in components from libraries in your React project. Using const is another way to do it that works better in Codepen. Do not be concerned about the difference at this time.
+// You've used import' on components from libraries in your React project.
+// Using const is another way to do it that works better in Codepen..
 
 /* Data */
-const INSTRUCTORS = [{
-	name: 'Michael Steinberg',
-  id: 0,
-	color: 'blue',
-  course: 'React',
-}, {
-	name: 'James Pritchett',
-  id: 1,
-	color: 'green',
-  course: 'React',
-}, {
-	name: 'Mathew Moser',
-  id: 2,
-	color: 'orange',
-  course: 'React',
-}, {
-  name: 'Brent Schneider',
-  id: 3,
-  color: 'purple',
-  course: 'React',
-}];
+const INSTRUCTORS = [
+  {
+    name: 'Michael Steinberg',
+    id: 0,
+    color: 'blue',
+    course: 'React',
+  },
+  {
+    name: 'James Pritchett',
+    id: 1,
+    color: 'green',
+    course: 'React',
+  },
+  {
+    name: 'Mathew Moser',
+    id: 2,
+    color: 'orange',
+    course: 'React',
+  },
+  {
+    name: 'Brent Schneider',
+    id: 3,
+    color: 'purple',
+    course: 'React',
+  }
+];
 
-/* ------ START PAYING ATTENTION HERE ------------------------------------- */
+/** REACT COMPONENTS
+ * Components are able to get an app's state as props from the Redux Store when:
+ *   1) They are nested inside a component that is wrapped in a <Provider> that is connected to a store (created somewhere with createStore)
+ *   2) The components themselves are subscribed to the store
+ * Generally you will not use store.subscribe() directly
+ * Instead you will use connect() with mapStateToProps()
+ * which handles subscribe() for you.
+ * If mapDispatchToProps() is also used with connect(), the component will be able to dispatch actions to the Redux Store as well.
+ */
 
-/* REACT COMPONENTS */
-/* A React component is able to get the application state as props from the Redux Store when it is:
-   1) Nested inside a component that is wrapped in a <Provider> that is connected to a store (created somewhere with createStore)
-   2) It is subscribed to the store - generally you will not use store.subscribe() directly, but instead you will use connect() with mapStateToProps(), which handles subscribe() for you.
-
-   If mapDispatchToProps() is also used with connect(), the component will be able to dispatch actions to the Redux Store as well. */
-
-class App extends React.Component {
+class App extends Component {
   render() {
+    const {addInstructor, instructors} = this.props;
     return (
       <div>
         <h1>Nucamp Instructors</h1>
         <hr />
-        <AddInstructor addInstructor={this.props.addInstructor} />
+        <AddInstructor className="Header" addInstructor={addInstructor} />
         <hr />
-        <InstructorsList instructors={this.props.instructors} />
+        <InstructorsList instructors={instructors} />
        </div>
      )
   }
 }
 
-class InstructorsList extends React.Component {
-
+class InstructorsList extends Component {
   render() {
     return (
-				<ul>
-					{this.props.instructors.map((instructor) => (
-
-						<li key={instructor.id}>
-							<Instructor instructor={instructor} />
-						</li>
+        <ul>
+          {this.props.instructors.map((instructor) => (
+            <li key={instructor.id}>
+              <Instructor
+                instructor={instructor}
+              />
+            </li>
             )
-					)}
+          )}
         </ul>
 
     )
   }
 }
 
-class AddInstructor extends React.Component {
-
+class AddInstructor extends Component {
   constructor(props) {
     super(props);
-
     this.inputName = null;
     this.inputCourse = null;
-
     this.setTextInputRef = inputElement => {
-      console.log(inputElement);
       switch(inputElement.id) {
         case "name": this.inputName = inputElement; break;
         case "course": this.inputCourse = inputElement; break;
+        default :;
       }
     };
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
+    if (!this.inputName.value || !this.inputCourse.value) {
+      return;
+    }
     this.props.addInstructor(this.inputName.value, this.inputCourse.value);
     e.target.reset();
   }
@@ -94,9 +102,9 @@ class AddInstructor extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label for="name">Name </label>
+        <label htmlFor="name">Name </label>
         <input id="name" type="text" ref={this.setTextInputRef} />
-        <label for="course"> Course Taught: </label>
+        <label htmlFor="course"> Course Taught: </label>
         <input id="course" type="text" ref={this.setTextInputRef} />
         <button type="submit"> Add Instructor</button>
       </form>
@@ -104,23 +112,23 @@ class AddInstructor extends React.Component {
   }
 }
 
-class Instructor extends React.Component {
+class Instructor extends Component {
   render = () => {
-    let style = {background: this.props.instructor.color};
+    const { color, name, course} = this.props.instructor;
+    let style = {background: color};
     return (
       <p
         style={style}
-        className="instructor">
-        {this.props.instructor.name} - {this.props.instructor.course}
+        className="instructor"
+      >
+        {name} - {course}
       </p>
     )
   }
 }
 
-
 /* ACTION TYPE */
 const ADD_INSTRUCTOR = 'ADD_INSTRUCTOR';
-
 
 /* ACTION */
 const addInstructor = (name, course) => ({
@@ -130,7 +138,6 @@ const addInstructor = (name, course) => ({
     course: course
   }
 });
-
 
 /* REDUCER */
 
@@ -146,7 +153,6 @@ const Reducer = (state = INSTRUCTORS, action) => {
   }
 };
 
-
 /* STORE & CONNECTIONS TO STORE */
 
 export const store = createStore(Reducer, INSTRUCTORS);
@@ -161,6 +167,11 @@ const mapDispatchToProps = dispatch => ({
   addInstructor: (name, course) => dispatch(addInstructor(name, course))
 });
 
-
-// The below line is written in a specific way to work with Codepen. Outside of Codepen, you would export default connect([args])(App).
+// The below line isn't written in a specific way to work with Codepen.
+// Inside of Codepen, you would call ReactDOM.render(
+  // <Provider store={store}>
+    // <App />
+  // </Provider>,
+  // document.getElementById("root")
+//)
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import './index.css';
 import App, { store } from './App';
 import * as serviceWorker from './serviceWorker';
 
 /* FINAL RENDER - here, we have wrapped our root component with <Provider>  */
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
